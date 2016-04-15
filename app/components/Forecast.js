f74499cd76237afdc7956c52541b89dfc2006945
Iting <wenit123@gmail.com>
2016-04-15 14:25:42 -0700

    work

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -42,7 +42,7 @@ function DayItem (props) {
   var icon = props.day.weather[0].icon;
   return (
     <div style={styles.dayContainer}>
-      <img style={styles.weather} src={'./app/images/weather-icons/' + icon + '.svg'} alt='Weather' />
+      <img style={styles.weather} src={'./app/images/icon/' + icon + '.svg'} alt='Weather' />
       <h2 style={styles.subheader}>{date}</h2>
     </div>
   )
